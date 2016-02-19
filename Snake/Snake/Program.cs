21d95cc5646369e035dc2821ff7109a8eddcb267
Lovchiev <lovchiev@mailinator.com>
2016-02-19 20:25:01 +0600

    Сделали инкапсуляцию

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -11,16 +11,10 @@ namespace Snake
         static void Main(string[] args)
         {
 
-            Point p1 = new Point();
-            p1.x = 5;
-            p1.y = 6;
-            p1.sym = '*';
+            Point p1 = new Point(5,6,'*');
             p1.Draw();
 
-            Point p2 = new Point();
-            p2.x = 5;
-            p2.y = 7;
-            p2.sym = '!';
+            Point p2 = new Point(6,7,'@');
             p2.Draw();
 
             Console.ReadLine();
